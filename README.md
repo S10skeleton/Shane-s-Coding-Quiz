e464fd6b062243618421a81bf50ba3544f1027d9
Shane <s10skeleton@gmail.com>
2023-12-15 00:28:54 -0700

    README update and final adjustments

--- README.md
+++ README.md
@@ -1,13 +1,24 @@
 # Shanes-Coding-Quiz
 
+## Task
+
+AS A coding boot camp student
+I WANT to take a timed quiz on JavaScript fundamentals that stores high scores
+SO THAT I can gauge my progress compared to my peers
+
 ## Description
 
-    Simple application to quiz the user on Javascript. 
-7 questions with a time limit and when you answer incorrectly
+Simple application to quiz the user on Javascript. 
+6 questions with a time limit and when you answer incorrectly
 it will penalize you with a larger time deduction.
 
-## Application Link 
+## Application and Github Links 
+
+https://s10skeleton.github.io/Shanes-Coding-Quiz/ 
+
+https://github.com/S10skeleton/Shanes-Coding-Quiz
 
 ## Screenshot
 
+![screenshot](./assets/images/Screenshot.png)
 
--- assets/script.js
+++ assets/script.js
@@ -3,8 +3,7 @@
 let currentQuestionIndex = 0;
 let timer;
 let timeLeft = 60; // time limit
-let finalScore;
-
+let finalScore = 0;
 
 // funtion to hide start button after clicked and show first question
 // it will also start the timer
@@ -16,7 +15,7 @@ function startQuiz() {
       clearInterval(timer);
       endQuiz();
     } else {
-      document.getElementById("timer").innerText = 'Time Left: ' + timeLeft;
+      document.getElementById("timer").innerText = "Time Left: " + timeLeft;
     }
   }, 1000);
   showQuestion();
@@ -24,37 +23,38 @@ function startQuiz() {
 
 document.getElementById("start-btn").addEventListener("click", startQuiz);
 
-// Questions array
+// List of questions
 const questions = [
   {
-    question: "What is 1?",
-    answers: ["yes", "no", "maybe", "IDK"],
+    question: "Inside which HTML element do we put the JavaScript?",
+    answers: ["<script>", "<scripting>", "<javascript>", "<js>"],
+    correct: 0,
+  },
+  {
+    question: "Where is the correct place to insert a JavaScript?",
+    answers: ["Both the <head> and <body>", "The <head> section", "The <body> section", "The very top"],
+    correct: 2,
+  },
+  {
+    question: "How to write an IF statement for executing some code if 'i' is NOT equal to 5?",
+    answers: ["if(i != 5)", "if (i <> 5)", "if i <> 5", "if i =! 5 then"],
+    correct: 0,
+  },
+  {
+    question: "How can you add a comment in a JavaScript?",
+    answers: ["<!--This is a comment-->", "//This is a comment", "'This is a comment", "#??This is a comment??"],
     correct: 1,
   },
   {
-    question: "What is 2?",
-    answers: ["yes", "no", "maybe", "IDK"],
-    correct: 1,
+    question: "How do you declare a JavaScript variable?",
+    answers: [ "variable carName;", "v carName;", "set carName;", "var carName;"],
+    correct: 3,
   },
   {
-    question: "What is 3?",
-    answers: ["yes", "no", "maybe", "IDK"],
-    correct: 1,
+    question: "Which operator is used to assign a value to a variable?",
+    answers: ["-", "x", "=", "#"],
+    correct: 2,
   },
-  {
-    question: "What is 4?",
-    answers: ["yes", "no", "maybe", "IDK"],
-    correct: 1,
-  },
-  {
-    question: "What is 5?",
-    answers: ["yes", "no", "maybe", "IDK"],
-    correct: 1,
-  },
-
-  // Add your question objects here
-  // Example: { question: "What is X?", answers: ["Option1", "Option2", "Option3", "Option4"], correct: 1 },
-  // Note: 'correct' is the index of the correct answer in the 'answers' array
 ];
 
 function showQuestion() {
@@ -76,7 +76,7 @@ function showQuestion() {
 
 function handleAnswer(isCorrect) {
   if (!isCorrect) {
-      timeLeft -= 5; // Apply penalty
+    timeLeft -= 5; // Apply penalty
   }
 
   displayResult(isCorrect); // Display the result message
@@ -84,14 +84,13 @@ function handleAnswer(isCorrect) {
   // Disable answer buttons to prevent multiple selections
   const answersButtons = document.querySelectorAll(".answer");
   answersButtons.forEach((button) => {
-      button.disabled = true;
+    button.disabled = true;
   });
 
   // Show next button
   document.getElementById("next-btn").style.display = "block";
 }
-
-
+// display results color coded to correct or wrong answer 
 function displayResult(isCorrect) {
   const resultElement = document.getElementById("result-message");
   resultElement.innerText = isCorrect ? "Correct!" : "Wrong!";
@@ -100,10 +99,10 @@ function displayResult(isCorrect) {
   if (isCorrect) {
     resultElement.classList.add("correct-answer");
     resultElement.classList.remove("wrong-answer");
-} else {
+  } else {
     resultElement.classList.add("wrong-answer");
     resultElement.classList.remove("correct-answer");
-}
+  }
 }
 document.getElementById("next-btn").addEventListener("click", nextQuestion);
 
@@ -112,26 +111,25 @@ function nextQuestion() {
   currentQuestionIndex++;
 
   if (currentQuestionIndex < questions.length) {
-      showQuestion();
+    showQuestion();
   } else {
     finalScore = timeLeft;
     endQuiz();
-  }};
-  
-  // Hide next button until the next answer is selected
-  document.getElementById("next-btn").style.display = "none";
-   
+  }
+}
 
-
+// Hide next button until the next answer is selected
+document.getElementById("next-btn").style.display = "none";
 
 function endQuiz() {
   clearInterval(timer);
-  document.getElementById("results"
-  ).innerText = `Quiz finished! Your score: ${timeLeft}`;
-  document.getElementById('next-btn').style.display = 'none';
+  document.getElementById(
+    "results"
+  ).innerText = `Quiz finished! Your score: ${finalScore}`;
+  document.getElementById("next-btn").style.display = "none";
   document.getElementById("score-submission").style.display = "block"; // Show score submission
 
-  // Hide the quiz container or show a restart button
+
 }
 
 // Hide the quiz container initially
@@ -144,16 +142,16 @@ document.getElementById("start-btn").addEventListener("click", () => {
   startQuiz();
 });
 
-// function to restart the quiz at any time 
+// function to restart the quiz at any time
 
-document.getElementById('restart').addEventListener('click', function() {
+document.getElementById("restart").addEventListener("click", function () {
   location.reload();
 });
 
-// function to store current score into high score list 
+// function to store current score into high score list
 function submitScore() {
   const userName = document.getElementById("user-name").value;
-  const newScore = { name: userName, score: finalScore};
+  const newScore = { name: userName, score: finalScore };
   const scores = JSON.parse(localStorage.getItem("topScores")) || [];
   scores.push(newScore);
   scores.sort((a, b) => b.score - a.score); // Sort scores in descending order
@@ -164,17 +162,15 @@ function submitScore() {
 }
 
 document.getElementById("submit-score").addEventListener("click", submitScore);
-// fuction to display the top 5 highscores 
+// fuction to display the top 5 highscores
 function displayTopScores() {
   const scores = JSON.parse(localStorage.getItem("topScores")) || [];
   const scoresList = document.getElementById("top-scores");
-  scoresList.innerHTML = ''; // Clear existing list
+  scoresList.innerHTML = ""; // Clear existing list
 
-  scores.forEach(score => {
-      const scoreElement = document.createElement("li");
-      scoreElement.innerText = `${score.name}: ${score.score}`;
-      scoresList.appendChild(scoreElement);
+  scores.forEach((score) => {
+    const scoreElement = document.createElement("li");
+    scoreElement.innerText = `${score.name}: ${score.score}`;
+    scoresList.appendChild(scoreElement);
   });
 }
-
-
